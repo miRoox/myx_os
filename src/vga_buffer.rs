82d92ebb7091bbdc6748e5f7c9264abf086b4ac9
miroox <miroox@outlook.com>
2020-12-02 18:52:21 +0800

    spin mutex

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,6 @@
 use core::fmt;
 use lazy_static::lazy_static;
+use spin::Mutex;
 
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
@@ -93,11 +94,11 @@ impl Writer {
 }
 
 lazy_static! {
-    pub static ref WRITER: Writer = Writer {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+    });
 }
 
 #[allow(dead_code)]
