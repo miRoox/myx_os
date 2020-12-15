284a24b8ae83431d9c85df0ec45e33635cedc5bd
miroox <miroox@outlook.com>
2020-12-15 20:54:40 +0800

    splite off a library from the main.rs,
    which can be included by other crates and integration test executables.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,13 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(myx_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 mod serial;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-        core::hint::unreachable_unchecked()
-    }
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -46,35 +29,9 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
+    myx_os::test_panic_handler(info)
 }
 
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
 
 #[test_case]
 fn trivial_assertion() {
